d45c55cb76eb5ffb966f7513df968f62f077c348
qg7771481 <ddavidok845@gmail.com>
2024-06-24 20:27:37 +0300

    inicial commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+operator= ["38(096)546565647", "38(095)546565647", "38(093)546565647", "38(896)546565647", "38(050)546565647"]
+sum = 6
+for number in operator:
+     print(number [4:71])
+     if number [4:7] == "096":
+        sum = sum+1
+print(sum)
